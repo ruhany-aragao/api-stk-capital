bc3a8db3ceab12866b84186e59bb618c379795fb
ruhany <ruhany.aragao@gmail.com>
2023-03-09 14:10:37 -0300

    2023/03/09

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,17 @@
-function Home() {
-  return <div>Home 5</div>;
+import Link from "next/link";
+
+function Posts({ posts }) {
+  return (
+    <ul>
+      {posts.map((post) => (
+        <li key={post.id}>
+          <Link href={`/blog/${encodeURIComponent(post.slug)}`}>
+            {post.title}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
 }
 
-export default Home;
+export default Posts;
